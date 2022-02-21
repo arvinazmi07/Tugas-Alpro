317e5eadc7ef5ba1ff71a0a32c20c3a121b73405
arvinazmi07 <100138408+arvinazmi07@users.noreply.github.com>
2022-02-21 21:13:56 +0700

    Update ProblemSet03.java

--- ProblemSet03/src/problemset03/ProblemSet03.java
+++ ProblemSet03/src/problemset03/ProblemSet03.java
@@ -1,22 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ProblemSet03;
 
-/**
- *
- * @author arvin azmi sava
- */
+
 import javax.swing.JOptionPane;
 public class ProblemSet03 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         
-        // TODO code application logic here
         String nama, input, alamat, hobi;
         int umur, tahunLahir, intensitasOlahraga,makan,lingkarPinggang, lingkarPinggul;
         double berat,tinggi,BBI,WHR ;
